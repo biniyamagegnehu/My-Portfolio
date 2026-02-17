8d504fec50b626857b2981cc1308c9cde76da15f
biniyamagegnehu <biniyamagegnehu2@gmail.com>
2026-02-17 18:14:21 +0300

    Change Instagram URL in Footer component
    Updated Instagram URL to the new account.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
 
   const socialLinks = [
     { name: 'GitHub', url: 'https://github.com/biniyamagegnehu', icon: 'fab fa-github' },
-    { name: 'Instagram', url: 'https://www.instagram.com/biniyamagegnehu', icon: 'fab fa-instagram' },
+    { name: 'Instagram', url: 'https://www.instagram.com/bini_yam19', icon: 'fab fa-instagram' },
     { name: 'Telegram', url: 'https://t.me/@Bbini19', icon: 'fab fa-telegram' },
     { name: 'LinkedIn', url: 'https://www.linkedin.com/in/biniyamagegnehu/', icon: 'fab fa-linkedin' }
   ]
